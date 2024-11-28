723340f0bff0c59031ea7e97bb98ad97271059a1
LucianoCarrizoMC <lucianocarrizo022@gmail.com>
2024-11-28 12:24:42 -0300

    qwe

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>hola</h1>
+      <p
+        style={{backgroundColor:"blue"} }>como estas?
+      </p>
+      <input type="color"
+      className="claseImaguinaria"
+      />oomaigaa
+    </>
+  );
+}
